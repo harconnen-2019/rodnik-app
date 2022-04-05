c52ee6699cbecdc631a032a60b83c7f4a9db2f23
Harconnen <harconnen.public@gmail.com>
2022-04-05 12:05:27 +0300

    Добавлен цвет текста

--- components/ui/button-theme-toggle/index.tsx
+++ components/ui/button-theme-toggle/index.tsx
@@ -3,7 +3,11 @@ type Props = {
     theme: string;
     setTheme: Dispatch<string>;
 };
+
+//FIXME: При первом запуске иконка всегда светлой темы
+
 const ButtonThemeToggle: FC<Props> = ({ theme, setTheme }) => {
+    // if (!theme) return null;
     return (
         <div
             className="w-10 h-10 md:w-16 md:h-16 bg-l-toggle hover:bg-l-h-toggle dark:bg-d-toggle dark:hover:bg-d-h-toggle rounded-b-full flex justify-center items-center absolute right-2 lg:right-24 2xl:right-32 cursor-pointer"
