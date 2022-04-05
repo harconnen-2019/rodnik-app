c94bef2350bee2970097012be77a2be07b3c0fcb
margo <margo.simik@yandex.ru>
2022-04-05 21:21:20 +0300

    верстка карточек номеров

--- components/nomer/index.tsx
+++ components/nomer/index.tsx
@@ -1,28 +1,44 @@
 import { FC } from 'react';
 import Image from 'next/image';
-// import Link from 'next/link';
+import Link from 'next/link';
 
 import { INomer } from '../../models/i-nomer';
 
 type Props = {
     nomer: INomer;
+    href: INomer;
 };
 
 const Nomer: FC<Props> = ({ nomer }) => {
     return (
         <>
-            <div className="w-80  h-56">
-                <Image
-                    src={`/static/img/nomera/${nomer.photo[0]}`}
-                    alt="Picture of the author"
-                    width={1200}
-                    height={900}
-                    // layout="fill"
-                />
-                <div className="mt-10">
-                    {nomer.corpus} {nomer.name}
-                </div>
-            </div>
+            <Link href={nomer.href}>
+                <a>
+                    <div className="relative shadow-xl  rounded-2xl hover:shadow-2xl transition duration-300 ease-in-out ">
+                        <Image
+                            className="rounded-2xl "
+                            src={`/static/img/nomera/${nomer.photo[0]}`}
+                            alt="Picture of the author"
+                            width={1200}
+                            height={900}
+                            // layout="fill"
+                        />
+                        <div className="bg-white dark:bg-d-page p-4 rounded-b-2xl  absolute inset-x-0 bottom-0 h-24">
+                            <div className="flex  flex-row justify-between ">
+                                <p className="w-48  text-lg line-clamp font-bold">
+                                    {nomer.name}
+                                </p>
+                                <div className="">
+                                    <span className="ml-2 text-2xl">
+                                        3700 ₽
+                                    </span>
+                                    <p>за к/место</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </Link>
         </>
     );
 };
--- pages/nomera.tsx
+++ pages/nomera.tsx
@@ -17,8 +17,11 @@ const Nomera: NextPage = () => {
                     content={`Номерной фонд, курортный отдых в лечебно-профилактическом санаторий «РОДНИК» по низким ценам в Пятигорске. Наш телефон: ${PRIMARY_PHONE}`}
                 />
             </Head>
-            <h1>Номерной фонд санатория</h1>
-            <div className="lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 grid gap-3">
+            <h1 className="mb-10 mt-10 text-center font-semibold">
+                Номерной фонд санатория
+            </h1>
+
+            <div className="lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 grid gap-x-5 gap-y-8">
                 {dataNomera.map((nomer) => (
                     <Nomer key={nomer.id} nomer={nomer} />
                 ))}
